d7f00b3f14a74dd887c3203efd10747d1d8b4dad
Tatiana Lipkina <tatianalipkina@MacBook-Pro-Tatiana.local>
2022-02-03 16:30:24 +0700

    another commit

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-var x = 10;
-var i = 4;
-
-function createFunction1() {
-    var x = 20;
-    var i = 12;
-    return new Function('return x;') ; // здесь x ссылается на глобальный x 
-    
-
-}
-
-function createFunction2() {
-    var x = 20 ;
-    function f() {
-        return x; // здесь x ссылается на локальный x и выше 
-
-    }
-    return f;
-
-}
+var heading = prompt('Please enter your name: ')
 
 
-var f1 = createFunction1()
-console.log(f1());
-var f2 = createFunction2();
-console.log(f2());
 
---
+++ function1.js
@@ -0,0 +1,30 @@
+var x = 10;
+var i = 4;
+
+function createFunction1() {
+    var x = 20;
+    var i = 12;
+    return new Function('return x;') ; // здесь x ссылается на глобальный x 
+    
+
+}
+fgf
+
+
+
+function createFunction2() {
+    var x = 20 ;
+    function f() {
+        return x; // здесь x ссылается на локальный x и выше 
+
+    }
+    return f;
+
+}
+
+
+var f1 = createFunction1()
+console.log(f1());
+var f2 = createFunction2();
+console.log(f2());
+
